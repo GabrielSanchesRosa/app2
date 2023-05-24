d7a6f6177422b0a2cccee544d6cc73598bc38f1d
Gabriel Sanches <gabrielsanches131202@gmail.com>
2023-05-23 22:36:47 -0300

    Implementando a servide de ofertas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  constructor(private ofertasService: OfertasService) {
+
+  }
+
+  ngOnInit() {
+    console.log(this.ofertasService.getOfertas())
+  }
 }
